721a8bced1114bcc443d3abb1d5e8fc917c88c7f
Lakshmi Narasimhan <badri.dilbert@gmail.com>
2017-07-07 18:11:43 +0530

    tsuruize PHP MySQL stack.

--- db.php
+++ db.php
@@ -17,31 +17,16 @@
 -->
 
 <?php
-
-if(!$_ENV["VCAP_SERVICES"]){ //local dev
-    $mysql_server_name = "127.0.0.1:3306";
-    $mysql_username = "root";
-    $mysql_password = "";
-    $mysql_database = "test";
-} else { //running in Bluemix
-    $vcap_services = json_decode($_ENV["VCAP_SERVICES" ]);
-    if($vcap_services->{'mysql-5.5'}){ //if "mysql" db service is bound to this application
-        $db = $vcap_services->{'mysql-5.5'}[0]->credentials;
-    } 
-    else if($vcap_services->{'cleardb'}){ //if cleardb mysql db service is bound to this application
-        $db = $vcap_services->{'cleardb'}[0]->credentials;
-    } 
-    else { 
-        echo "Error: No suitable MySQL database bound to the application. <br>";
-        die();
-    }
-    $mysql_database = $db->name;
-    $mysql_port=$db->port;
-    $mysql_server_name =$db->hostname . ':' . $db->port;
-    $mysql_username = $db->username; 
-    $mysql_password = $db->password;
+if(!array_filter([$_ENV["MYSQL_HOST"], $_ENV["MYSQL_DATABASE"], $_ENV["MYSQL_PORT"], $_ENV["MYSQL_USER"], $_ENV["MYSQL_ROOT_PASSWORD"]]) {
+  echo "Error: No suitable MySQL database bound to the application. <br>";
+  die();
+} else {
+  $mysql_database = $_ENV["MYSQL_DATABASE"];
+  $mysql_server_name =$_ENV["MYSQL_HOST"] . ':' . $_ENV["MYSQL_PORT"];
+  $mysql_username = $_ENV["MYSQL_USER"];
+  $mysql_password = $_ENV["MYSQL_ROOT_PASSWORD"];
 }
-//echo "Debug: " . $mysql_server_name . " " .  $mysql_username . " " .  $mysql_password . "\n";
+
 
 $mysqli = new mysqli($mysql_server_name, $mysql_username, $mysql_password, $mysql_database);
 if ($mysqli->connect_errno) {
